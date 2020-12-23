be8dda7607646a364ed71502b21348ff7c801cd6
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-23 16:44:27 +0530

    Add. Added Use RANDOM for Attendance Check.

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,3 +1,11 @@
 #! /bin/bash -x
 
 echo "Welcome to Employee Wage Computation"
+num0=0
+randomCheck=$(( RANDOM % 2 ))
+if [ $randomCheck -eq $num0 ]
+then
+        echo "Employee is absent"
+else
+        echo "Employee is present"
+fi
