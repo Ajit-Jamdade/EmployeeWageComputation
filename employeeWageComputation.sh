ebf46f81ecc534aeb7b7d6c34158a9dc50dd327b
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-24 15:07:56 +0530

    Add. Added daily salary calculation

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,11 +1,16 @@
 #! /bin/bash -x
 
 echo "Welcome to Employee Wage Computation"
-num0=0
+isAbsent=0
+empWagePrHr=20
+empHrs=8
 randomCheck=$(( RANDOM % 2 ))
-if [ $randomCheck -eq $num0 ]
+if [ $randomCheck -eq $isAbsent ]
 then
-        echo "Employee is absent"
+   echo "Employee is absent"
+        echo "Daily salary is zero."
 else
-        echo "Employee is present"
+   echo "Employee is present"
+        salary=$(( empWagePrHr * empHrs ))
+   echo "Daily salary is "$salary
 fi
