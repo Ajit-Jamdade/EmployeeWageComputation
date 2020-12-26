57b387b651afc3624995af5ff8d582658458491f
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-26 15:19:20 +0530

    Add. Added calculation of Employee wages for a month.

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -2,22 +2,27 @@
 
 echo "Welcome to Employee Wage Computation"
 
-isAbsent=0
-isPartTime=1
-isFullTime=2
-empWagePrHr=20
-empCheck=$(( RANDOM % 3 ))
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empWagePerHr=20;
+numWorkingDays=20;
 
-case $empCheck in
-        $isFullTime)
-                empHrs=8
-                ;;
-        $isPartTime)
-                empHrs=4
-                ;;
-        *)
-                empHrs=0
-                ;;
-esac
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$((RANDOM%3))
+		case $empCheck in
+        	$isFullTime)
+        	        empHrs=8
+                	;;
+        	$isPartTime)
+                	empHrs=4
+                	;;
+        	*)
+                	empHrs=0
+                	;;
+		esac
 
-salary=$(( empWagePrHr * empHrs ))
+		salary=$(($empHrs*$empWagePerHr));
+		totalSalary=$(($totalSalary+$salary));
+done
